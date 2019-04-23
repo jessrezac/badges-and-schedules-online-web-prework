b9d3d33c82885a2d3b6545823282c34240448441
Jess Rezac <jess.rezac@gmail.com>
2019-04-23 03:14:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(conference_speakers)
+  badge_messages = []
+  conference_speakers.each do |speaker|
+    badge_messages.pop(badge_maker(speaker))
+  end
+  return badge_messages
+end
+
+def assign_rooms(conference_speakers)
+  rooms = [1..7]
+  conference_speakers
