ec60698f306a80c52b8c12201439665f70c30e95
Jess Rezac <jess.rezac@gmail.com>
2019-04-23 03:29:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,8 +22,6 @@ def assign_rooms(conference_speakers)
 end
 
 def printer
-  badge_messages = batch_badge_creator(conference_speakers)
   puts badge_messages
-  room_assignments = assign_rooms(conference_speakers)
   puts room_assignments
 end
